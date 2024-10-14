6dd415a1f6b472794e6ce180622231281791ed49
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-14 14:59:07 -0400

    Create Layout.tsx

---
+++ client/src/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+
+export default function Layout() {
+  return (
+    <>
+
+        <div className="">Desde Layout</div>
+
+
+
+        <Outlet />
+    </>
+  )
+}
