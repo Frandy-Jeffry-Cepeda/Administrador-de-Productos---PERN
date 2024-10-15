2d8559103b7f222a71a0d1a52e10eb8e99e76e34
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-15 07:44:24 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import router from './router'
+import cors, { CorsOptions } from 'cors'
 import swaggerUi  from 'swagger-ui-express'
 import swaggerSpec, { swaggerUiOptions } from './config/swagger'
 import db from './config/db'
@@ -22,6 +23,20 @@ connectDB()
 // Instancia de express
 
 const server = express()
+
+// Permitir conexiones
+
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL || 'http://localhost:4000/api/products'){
+            callback(null, true)
+        } else {
+            callback(new Error('Error de cors'))
+        }
+    }
+}
+
+server.use(cors(corsOptions))
 
 // Leer datos de formularios
 
